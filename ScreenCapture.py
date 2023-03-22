2a143a92fc5d00892576d43cbc74771f334b8d0f
unknown <pyungho.seo@gmail.com>
2023-03-22 14:42:16 +0900

    second commit

--- ScreenCapture.py
+++ ScreenCapture.py
@@ -1,12 +1,17 @@
+import os
 import time
 import pyautogui
 from PIL import Image
 
+# 저장할 경로 저장하기
+path = r"C:\ScreenCaptured"
+
 while True:
-    # 현재 화면을 캡쳐하여 Image 객체로 가져오기
-    screenshot = pyautogui.screenshot()
-    # 파일명에 현재 시각 포함하여 저장하기
-    filename = f"screenshot_{time.strftime('%Y%m%d_%H%M%S')}.png"
-    screenshot.save(filename)
+    # 현재 화면을 캡처하여 Image 객체로 가져오기
+    screenshot = pyautogui.screenshot(region=(0, 0, 1920, 1080))
+    # JPEG 이미지로 변환하여 파일로 저장하기
+    filename = f"screenshot_{time.strftime('%Y%m%d_%H%M%S')}.jpg"
+    # screenshot.save(filename, format="JPEG", quality=95)
+    screenshot.save(os.path.join(path, filename), format="JPEG", quality=100)
     # 1분 대기
-    time.sleep(60)
+    time.sleep(10)
